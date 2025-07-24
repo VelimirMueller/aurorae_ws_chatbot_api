f0e6d574e61155ae53ebd84a1120f1acaae97e59
VelimirMueller <velimir.mueller@googlemail.com>
2025-07-24 12:57:45 +0200

    add pre commit hook script

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from dotenv import dotenv_values
-
 # Initialize Flask
 from flask import Flask
 from flask_cors import CORS
@@ -7,43 +6,41 @@ from flask_sock import Sock
 
 from src.api import json_api_bp
 from src.chat import chat_session
-
 # Initialize model
 from src.model import ws_model
-
 # Initialize routes and api
 from src.routes import home_bp
 
 # load ..env variables
-environ = dotenv_values('.env')
+environ = dotenv_values(".env")
 
 # Initialize basic flask app with websockets and cors headers set
 app = Flask(__name__)
 cors = CORS(app)
 
-print('app initialised')
+print("app initialised")
 
 # Initialize routes
 app.register_blueprint(home_bp)
-print('routes initialised')
+print("routes initialised")
 
 # Initialize API
 app.register_blueprint(json_api_bp)
-print('api initialised')
+print("api initialised")
 
 # Initialize websocket
 sock = Sock(app)
 
 
-@sock.route('/api_ws')
+@sock.route("/api_ws")
 def api_ws(sock):
     with ws_model.chat_session():
         while True:
             chat_session(sock.receive(), ws_model, sock)
 
 
-print('websocket initialised: ' + str(sock))
+print("websocket initialised: " + str(sock))
 
 # Run automatically when executed as script but not when imported as module
 if __name__ == "__main__":
-    app.run(host=environ['HOST'], port=environ['PORT'])
+    app.run(host=environ["HOST"], port=environ["PORT"])
